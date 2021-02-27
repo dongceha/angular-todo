abaab5acc11737e44de57cc252895c68442c1c9d
dongceha <524970335@qq.com>
2021-02-27 15:56:14 +0800

    feat: init

---
+++ code/angular-test/src/app/store/actions/todo.actions.ts
@@ -0,0 +1,10 @@
+import { createAction, props } from '@ngrx/store';
+
+// 添加任务
+export const addTodo = createAction('addTodo');
+// 删除任务
+export const deleteTodo = createAction('deleteTodo');
+
+
+
+
